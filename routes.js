457e878f49f84b10662da416991d0df2e52a9c39
Casey Newell <cnewell187@gmail.com>
2016-11-11 11:41:36 -0700

    yo

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 module.exports = function(app){
-
+app.get('/', function(req,res){
+  res.send("Yo")
+})
   app.post('/attendance', function(req, res){
     console.log(req.body);
     res.send("Money No Change");
