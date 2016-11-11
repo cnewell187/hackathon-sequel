cf38c64fdedd87b0750decc2fbabbfd1752b28be
Casey Newell <cnewell187@gmail.com>
2016-11-11 12:06:39 -0700

    yo

--- controllers/users.js
+++ controllers/users.js
@@ -1,50 +1,54 @@
-var User = require('../models/usersModel'); // 
+var User = require('../models/usersModel'); //
 
-function create (req, res) {
+function create(req, res) {
 
     var newDoc = new User(req.body);
 
-    newDoc.save((err, doc)=>{
-        if(err){
+    newDoc.save((err, doc) => {
+        if (err) {
             return res.send(err);
         }
         res.send(doc);
     });
 }
 
-function get (req, res) {
+function get(req, res) {
     // get One
-    if(req.params.id){
-        User.findOne({_id : req.params.id}, (err, document)=>{
-            if(err){
+    if (req.params.id) {
+        User.findOne({
+            _id: req.params.id
+        }, (err, document) => {
+            if (err) {
                 // if(err.name === "CastError" && err.kind === "ObjectId"){
                 //     return res.send(`That ain't no ID`)
                 // }
 
                 return res.send(err);
             }
-            if(!document){
+            if (!document) {
                 return res.send('No one with that id')
             }
             res.send(document);
         });
     }
     // get Many
-    else{
+    else {
         User
             .find({})
             // .sort()
             // .limit()
             .populate('headquarter')
-            .exec((err, documents)=>{
-                if(err){
+            .exec((err, documents) => {
+                if (err) {
                     return res.send(err);
                 }
                 res.send(documents);
             })
+    }
+}
 
 
 module.exports = {
-    create : create,
-    get    : get,
+    create: create,
+    get: get,
 }
