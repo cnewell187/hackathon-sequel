58f3c0ae259845a5c97a0c6c41604b4b8c36fd27
Casey Newell <cnewell187@gmail.com>
2016-11-11 13:22:39 -0700

    yo momma

--- controllers/users.js
+++ controllers/users.js
@@ -37,35 +37,36 @@ function get(req, res) {
 
 function updateStudent(req, res) {
 
-   User.findOne({slackName: req.body.user_name}, function(err, userDoc) {
-       if (err) {
-          return res.send("Daves not here man");
-       }
-       if (!userDoc) {
-           return res.send('No man its Dave')
-       }
-       res.send(userDoc);
-   });)
-
+    User.findOne({
+        slackName: req.body.user_name
+    }, function(err, userDoc) {
+        if (err) {
+            return res.send("Daves not here man");
+        }
+        if (!userDoc) {
+            return res.send('No man its Dave')
+        }
+        res.send(userDoc);
+    });
 
 }
 
 function newStudent(req, res) {
-  console.log(req.body)
-  var info = {
-      realName: req.body.text,
-      slackName: req.body.user_name,
-      timestamps: [],
-      channelName: req.body.channel_name
-  }
-  var newDoc = new User(info);
+    console.log(req.body)
+    var info = {
+        realName: req.body.text,
+        slackName: req.body.user_name,
+        timestamps: [],
+        channelName: req.body.channel_name
+    }
+    var newDoc = new User(info);
 
-  newDoc.save((err, doc) => {
-      if (err) {
-          return res.send(err);
-      }
-      res.send(doc);
-  });
+    newDoc.save((err, doc) => {
+        if (err) {
+            return res.send(err);
+        }
+        res.send(doc);
+    });
 }
 
 
