4e2e6ba184a411143a71500013bba2d70540cb13
Casey Newell <cnewell187@gmail.com>
2016-11-11 12:09:04 -0700

    yo

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
 
     app.get('/', (req, res) => {
         res.sendFile('index.html', {
-            root: './public/html'
+            root: './public'
         })
     });
 
