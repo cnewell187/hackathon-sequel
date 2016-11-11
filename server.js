29af86807335c545eec5702f6e4c85569a41f9e6
Casey Newell <cnewell187@gmail.com>
2016-11-11 11:23:06 -0700

    message

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ var express = require('express'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    Routes = require('./routes');
+    Routes = require('./routes'),
+    HTTP = require('http'),
+    HTTPS = require('https'),
+    ports = {
+        http:  process.env.PORT || 80,      // default HTTP port
+        https: process.env.PORT_SSL || 443  // defualt HTTPS port
+    },
+    httpsConfig = { // https://nodejs.org/api/https.html
+         key:  fs.readFileSync('/etc/letsencrypt/live/marte.io/privkey.pem'),
+         cert: fs.readFileSync('/etc/letsencrypt/live/marte.io/cert.pem')
+    };
 
 mongoose.connect('mongodb://localhost/hackathon-sequel');
 
@@ -13,6 +23,8 @@ var app = express();
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}), bodyParser.json());
+
+
 
 // Routes
 Routes(app);
